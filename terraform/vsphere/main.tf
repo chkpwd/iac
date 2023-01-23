62c6d6c7ecef463e4aad16b795dfbaa1a533ae84
chkpwd <mentality@protonmail.com>
2023-01-23 12:06:49 -0500

    changed how modules are handled

--- terraform/vsphere/main.tf
+++ terraform/vsphere/main.tf
@@ -1,14 +1,29 @@
-module "mirage" {
-  source                    = "./modules/mirage"
-  vm_name                   = "mirage"
-  vm_cpu                    = 1
-  vm_ram                    = 1024
+module "vm" {
+  source                    = "./modules/vm"
+  vm_name                   = "horizon"
+  vm_cpu                    = 2
+  vm_ram                    = 4096
   vm_template               = "deb-x11-template-test"
-  vm_ip                     = "172.16.16.65"
+  vm_ip                     = "172.16.16.59"
   vm_netmask                = "24"
   vm_gateway                = "172.16.16.1"
   vm_dns                    = [0,1]
-  vm_public_key             = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICsJocZS/OZ/4ZrLAxFOppiVMTym5oDkfHiir3YFg8mQ endeavourOS-test"
+  vm_public_key             = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICsJocZS/OZ/4ZrLAxFOppiVMTym5oDkfHiir3YFg8mQ endeavourOS"
+  vsphere_user              = var.vsphere_user
+  vsphere_password          = var.vsphere_password
+}
+
+module "vm" {
+  source                    = "./modules/vm"
+  vm_name                   = "mirage"
+  vm_cpu                    = 2
+  vm_ram                    = 8192
+  vm_template               = "deb-x11-template-test"
+  vm_ip                     = "172.16.20.102"
+  vm_netmask                = "24"
+  vm_gateway                = "172.16.16.1"
+  vm_dns                    = [0,1]
+  vm_public_key             = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICsJocZS/OZ/4ZrLAxFOppiVMTym5oDkfHiir3YFg8mQ endeavourOS"
   vsphere_user              = var.vsphere_user
   vsphere_password          = var.vsphere_password
 }
