dcf4ce6667d08616347c9ec93aac86f3da57bd15
chkpwd <mentality@protonmail.com>
2023-02-02 22:55:33 -0500

    testing vm added as a module

--- terraform/vsphere/main.tf
+++ terraform/vsphere/main.tf
@@ -62,6 +62,22 @@ module "crypto" {
   vsphere_password          = var.vsphere_password
 }
 
+module "test-vm" {
+  source                    = "./modules/vm-code-server"
+  vm_name                   = "test-vm"
+  vm_cpu                    = 1
+  vm_ram                    = 1024
+  vm_network                = "LAN"
+  vm_template               = "deb-x11-template"
+  vm_ip                     = "172.16.16.65"
+  vm_netmask                = "24"
+  vm_gateway                = "172.16.16.1"
+  vm_dns                    = var.vm_dns
+  vm_public_key             = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBK2VnKgOX7i1ISETheqjAO3/xo6D9n7QbWyfDAPsXwa hyoga@lifeline"
+  vsphere_user              = var.vsphere_user
+  vsphere_password          = var.vsphere_password
+}
+
 module "mirage" {
   source                    = "./modules/vm"
   vm_name                   = "mirage"
