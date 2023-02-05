81648fd97611e79a27ce4a051f2c0a639f22b3ae
chkpwd <mentality@protonmail.com>
2023-02-05 13:39:11 -0500

    setup HA server

--- terraform/vsphere/main.tf
+++ terraform/vsphere/main.tf
@@ -14,22 +14,6 @@ module "horizon" {
   vsphere_password          = var.vsphere_password
 }
 
-module "valkyrie" {
-  source                    = "./modules/vm"
-  vm_name                   = "valkyrie"
-  vm_cpu                    = 1
-  vm_ram                    = 512
-  vm_network                = "LAN"
-  vm_template               = "deb-x11-template"
-  vm_ip                     = "172.16.16.11"
-  vm_netmask                = "24"
-  vm_gateway                = "172.16.16.1"
-  vm_dns                    = var.vm_dns
-  vm_public_key             = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBK2VnKgOX7i1ISETheqjAO3/xo6D9n7QbWyfDAPsXwa hyoga@lifeline"
-  vsphere_user              = var.vsphere_user
-  vsphere_password          = var.vsphere_password
-}
-
 module "stable-diffusion" {
   source                    = "./modules/vm"
   vm_name                   = "stable-diffusion"
@@ -63,23 +47,6 @@ module "crypto" {
   ssh_password              = var.ssh_password
 }
 
-module "test-vm" {
-  source                    = "./modules/vm-code-server"
-  vm_name                   = "test-vm"
-  vm_cpu                    = 1
-  vm_ram                    = 1024
-  vm_network                = "LAN"
-  vm_template               = "deb-x11-template"
-  vm_ip                     = "172.16.16.65"
-  vm_netmask                = "24"
-  vm_gateway                = "172.16.16.1"
-  vm_dns                    = var.vm_dns
-  vm_public_key             = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBK2VnKgOX7i1ISETheqjAO3/xo6D9n7QbWyfDAPsXwa hyoga@lifeline"
-  vsphere_user              = var.vsphere_user
-  vsphere_password          = var.vsphere_password
-  ssh_password              = var.ssh_password
-}
-
 module "mirage" {
   source                    = "./modules/vm"
   vm_name                   = "mirage"
@@ -96,3 +63,18 @@ module "mirage" {
   vsphere_password          = var.vsphere_password
 }
 
+module "homeassistant" {
+  source                    = "./modules/vm"
+  vm_name                   = "valkyrie"
+  vm_cpu                    = 2
+  vm_ram                    = 4096
+  vm_network                = "LAN"
+  vm_template               = "deb-x11-template"
+  vm_ip                     = "172.16.16.11"
+  vm_netmask                = "24"
+  vm_gateway                = "172.16.16.1"
+  vm_dns                    = var.vm_dns
+  vm_public_key             = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBK2VnKgOX7i1ISETheqjAO3/xo6D9n7QbWyfDAPsXwa crypto"
+  vsphere_user              = var.vsphere_user
+  vsphere_password          = var.vsphere_password
+}
--- terraform/vsphere/outputs.tf
+++
@@ -1,6 +0,0 @@
-# Output variable definitions
-
- output "vm_internal_address" {
-   description = "IDs of the VPC's internal subnets"
-   value       = module.test-vm.vsphere_virtual_machine.standalone.vm_name
-}
