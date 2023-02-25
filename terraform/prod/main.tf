c513c804f3d1f1cc6c456b4f7c1fabde991660ac
chkpwd <mentality@protonmail.com>
2023-02-25 00:14:58 -0500

    chore!: Dropping test guest machines from master

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -113,23 +113,3 @@ module "kubes-worker-nodes" {
   vsphere_user              = var.vsphere_user
   vsphere_password          = var.vsphere_password
 }
-# module "rocky-linux" {
-#   source                    = "../modules/vultr"
-#   vultr_api_key             = var.vultr_api_key
-# }
-
-# module "proxmox-vm" {
-#  source      = "../modules/proxmox"
-#  vm_count = 3
-#  ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICsJocZS/OZ/4ZrLAxFOppiVMTym5oDkfHiir3YFg8mQ"
-#  vm_user = "hyoga"
-#  ip_address = "172.16.16.20"
-#  gateway = "172.16.16.1"
-#  bridge = "vmbr0"
-#  disk_size = 15
-#  cpu_count = 2
-#  ram_size = 2048
-#  storage_location = "Arenas"
-#  vlan_tag = "0"
-#  vm_name = "ansible-vm"
-# }
