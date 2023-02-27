4743ec8f8cf48ddbe30939f66b260f8d56185c19
chkpwd <mentality@protonmail.com>
2023-02-27 12:50:42 -0500

    chore(state):  added veeam testing

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,7 +1,7 @@
-module "kubes-control-plane" {
+module "veeam-backup" {
   source                    = "../modules/vsphere"
   for_each                  = toset(["1", "2", "3"])
-  vm_name                   = "kubes-master-${each.key}"
+  vm_name                   = "node-${each.key}"
   vm_cpu                    = 2
   vm_ram                    = 2048
   vm_network                = "LAN"
@@ -15,23 +15,40 @@ module "kubes-control-plane" {
   vsphere_password          = var.vsphere_password
 }
 
-module "kubes-worker-nodes" {
-  source                    = "../modules/vsphere"
-  for_each                  =  toset([for n in range(4, 6) : tostring(n)])
-  vm_name                   = "kubes-worker-${each.key}"
-  vm_cpu                    = 2
-  vm_ram                    = 2048
-  vm_network                = "LAN"
-  vm_template               = "deb-x11-template"
-  vm_ip                     = "172.16.16.23${each.key}"
-  #vm_ip                     = "172.16.16.23${format("%2d", each.value + 1)}"
-  vm_netmask                = "24"
-  vm_gateway                = "172.16.16.1"
-  vm_dns                    = var.vm_dns
-  vm_public_key             = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBK2VnKgOX7i1ISETheqjAO3/xo6D9n7QbWyfDAPsXwa crypto"
-  vsphere_user              = var.vsphere_user
-  vsphere_password          = var.vsphere_password
-}
+# module "kubes-control-plane" {
+#   source                    = "../modules/vsphere"
+#   for_each                  = toset(["1", "2", "3"])
+#   vm_name                   = "kubes-master-${each.key}"
+#   vm_cpu                    = 2
+#   vm_ram                    = 2048
+#   vm_network                = "LAN"
+#   vm_template               = "deb-x11-template"
+#   vm_ip                     = "172.16.16.23${each.key}"
+#   vm_netmask                = "24"
+#   vm_gateway                = "172.16.16.1"
+#   vm_dns                    = var.vm_dns
+#   vm_public_key             = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBK2VnKgOX7i1ISETheqjAO3/xo6D9n7QbWyfDAPsXwa crypto"
+#   vsphere_user              = var.vsphere_user
+#   vsphere_password          = var.vsphere_password
+# }
+
+# module "kubes-worker-nodes" {
+#   source                    = "../modules/vsphere"
+#   for_each                  =  toset([for n in range(4, 6) : tostring(n)])
+#   vm_name                   = "kubes-worker-${each.key}"
+#   vm_cpu                    = 2
+#   vm_ram                    = 2048
+#   vm_network                = "LAN"
+#   vm_template               = "deb-x11-template"
+#   vm_ip                     = "172.16.16.23${each.key}"
+#   #vm_ip                     = "172.16.16.23${format("%2d", each.value + 1)}"
+#   vm_netmask                = "24"
+#   vm_gateway                = "172.16.16.1"
+#   vm_dns                    = var.vm_dns
+#   vm_public_key             = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBK2VnKgOX7i1ISETheqjAO3/xo6D9n7QbWyfDAPsXwa crypto"
+#   vsphere_user              = var.vsphere_user
+#   vsphere_password          = var.vsphere_password
+# }
 # module "rocky-linux" {
 #   source                    = "../modules/vultr"
 #   vultr_api_key             = var.vultr_api_key
