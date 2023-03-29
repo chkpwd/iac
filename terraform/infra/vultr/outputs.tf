a0a2194567a8ffa0ec0008bce956df68f1901a7a
chkpwd <mentality@protonmail.com>
2023-03-29 13:25:13 -0400

    fix(terraform): fixed outputs using wildcard

--- terraform/infra/vultr/outputs.tf
+++ terraform/infra/vultr/outputs.tf
@@ -1,12 +1,19 @@
-output "hostname" {
+output "fedora_hats_hostname" {
   value = {
-    for instance in vultr_instance.instances :
-      instance.id => instance.hostname
+    for instance in vultr_instance.fedora_hats : instance.id => instance.hostname
   }
 }
-output "ipv6_address" {
+output "fedora_hats_ipv6_address" {
   value = {
-    for instance in vultr_instance.instances :
-      instance.id => instance.main_ip
+    for instance in vultr_instance.fedora_hats : instance.id => instance.main_ip
   }
+
+}
+
+output "rocks_hostname" {
+  value = vultr_instance.rocks.*.hostname
+}
+
+output "rocks_main_ip" {
+  value = vultr_instance.rocks.*.hostname
 }
