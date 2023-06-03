5d7b1e8b6300f317ad74b27ad7072b9d89283110
unix-chkpwd <unix.chkpwd@gmail.com>
2023-06-03 09:55:12 -0400

    fix(ansible): calling groups

--- terraform/infra/ansible/k3s.tf
+++ terraform/infra/ansible/k3s.tf
@@ -2,11 +2,16 @@
 # Ansible Resources
 #===============================================================================
 
-resource "ansible_playbook" "playbook" {
-  groups     = ["docker_hosts"]
-  name       = "mirage"
-  playbook   = "/home/hyoga/code/boilerplates/ansible/playbooks/get_macaddress.yaml"
-  #replayable = true
-  #diff_mode  = true
-  vault_password_file = "/home/hyoga/code/boilerplates/ansible/vault-password"
+# resource "ansible_playbook" "playbook" {
+#   groups     = ["docker_hosts"]
+#   name       = "mirage"
+#   playbook   = "/home/hyoga/code/boilerplates/ansible/playbooks/get_macaddress.yaml"
+#   #replayable = true
+#   #diff_mode  = true
+#   vault_password_file = "/home/hyoga/code/boilerplates/ansible/vault-password"
+# } 
+
+resource "ansible_group" "group" {
+  name     = "k3s_cluster"
+  children = ["master"]
 }
