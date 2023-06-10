fb23473dc211b4b18e948ce2a481eed150b10236
unix-chkpwd <unix.chkpwd@gmail.com>
2023-06-10 13:39:57 -0400

    start(servarr): created variable map type instead

--- terraform/infra/servarr/vars.tf
+++ terraform/infra/servarr/vars.tf
@@ -1,6 +1,17 @@
-media_host_ip    = "172.16.10.20"
-sonarr_port      = "8989"
-prowlarr_port    = "9696"
-radarr_port      = "7878"
-sabnzbd_port     = "8180"
-qbittorrent_port = "8280"
+variable media_host_ip {
+  type = string
+  default = "172.16.10.20"
+  description = "Media Host IP Address"
+}
+
+variable "ports" {
+  type = map(string)
+  default = {
+    "sonarr"      = "8989"
+    "prowlarr"    = "9696"
+    "radarr"      = "7878"
+    "sabnzbd"     = "8180"
+    "qbittorrent" = "8280"
+  }
+  description = "Mapping of services to their respective ports"
+}
