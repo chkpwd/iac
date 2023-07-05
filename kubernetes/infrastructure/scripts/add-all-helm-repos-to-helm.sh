1e24bb259af7325eb072204c7160f2d102c95671
Bryan Jones <unix-chkpwd@gmail.com>
2023-07-05 13:26:33 -0400

    feat(helm): add workdir

--- kubernetes/infrastructure/scripts/add-all-helm-repos-to-helm.sh
+++ kubernetes/infrastructure/scripts/add-all-helm-repos-to-helm.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
-for file in ../flux-system/sources/helm-repos/*.yaml; do
+#!/usr/bin/env bash
+
+# Set working dir
+WORKDIR="/home/hyoga/code/boilerplates/kubernetes/infrastructure"
+
+# Change into kubernetes infra dir
+cd $WORKDIR
+
+for file in flux-system/sources/helm-repos/*.yaml; do
     name=$(yq -r '.metadata.name' $file);
     url=$(yq -r '.spec.url' $file);
 
