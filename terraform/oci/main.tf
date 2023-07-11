e8c42759f7d6c36145ad7420bbb4c8c345b6a843
Bryan Jones <unix-chkpwd@gmail.com>
2023-07-11 19:40:48 -0400

    add(oci, terraform): arm vm

--- terraform/oci/main.tf
+++ terraform/oci/main.tf
@@ -1,3 +1,31 @@
+module "ct-01-aarch64" {
+  source                         = "../_modules/oci_vm"
+  instance_spec                  = {
+    name                         = "ct-01-aarch64"
+    cpus                         = 1
+    memory_gb                    = 6
+    disk_size                    = 50
+    shape                        = "VM.Standard.A1.Flex" 
+    image_id                     = "ocid1.image.oc1.iad.aaaaaaaa7bvpvpgorsj2ciivlrxd4acxcjpbuhfv26pld3qnhjav5hpamm5q"
+    network                      = {
+      subnet_id                  = "${data.sops_file.oci-secrets.data["oci_subnet_ocid"]}"
+      vnic_label                 = "Primaryvnic"
+      hostname                   = "ct-01-aarch64"
+      assign_public_ip           = true
+      assign_private_dns_record  = true
+    }
+    ssh_authorized_keys          = "${data.sops_file.oci-secrets.data["ssh_pub_key"]}"
+  }
+
+  ssh_allowed_ips                = [
+    {
+      description                = "Public IP for homelab"
+      ip                         = "${data.sops_file.oci-secrets.data["public_address"]}/32"
+    }
+  ]
+  oci_availability_domain_number = 2
+}
+
 module "ct-01-x86" {
   source                         = "../_modules/oci_vm"
   instance_spec                  = {
