ead866d7ad6d7dc7503fe2093a2ea8fbd52381dd
Bryan Jones <unix.chkpwd@gmail.com>
2023-07-26 18:43:18 -0400

    chore(oci, terraform):  varible for pub key

--- terraform/oci/variables.tf
+++ terraform/oci/variables.tf
@@ -3,4 +3,8 @@ variable "private_key_path" {
   description = "Path to the Private Key for the OCI org"
 }
 
-variable "availability_domain" {}
+variable "main_pub_key" {
+  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILUG+qEDkI4NeBLLtyrSPfHhlhxjBwkdYGNECCX3JpLg oci"
+}
+
+# variable "availability_domain" {}
