3cd0514be4810b8aa05d0d72b2f23dfddda6e6cc
Bryan Jones <unix.chkpwd@gmail.com>
2023-07-26 18:35:19 -0400

    add(oci, cloudflare): A record for oci instances

--- terraform/cloudflare/a_records.tf
+++ terraform/cloudflare/a_records.tf
@@ -7,6 +7,15 @@ resource "cloudflare_record" "main" {
   zone_id = data.sops_file.cloudflare-secrets.data["cloudflare_zone_id"]
 }
 
+resource "cloudflare_record" "secondary" {
+  name    = "remote.chkpwd.com"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  value   = data.tfe_outputs.oci.values.node_public_ip
+  zone_id = data.sops_file.cloudflare-secrets.data["cloudflare_zone_id"]
+}
+
 resource "cloudflare_record" "www_main" {
   name    = "www"
   proxied = true
