6de7f6243485badc8d4d6fd1665f17c644e994e8
Bryan Jones <unix.chkpwd@gmail.com>
2023-07-30 11:00:03 -0400

    refactor(terraform, vsphere): removed unused data

--- terraform/vsphere/data.tf
+++ terraform/vsphere/data.tf
@@ -6,39 +6,3 @@ data "vsphere_datastore" "media_datastore" {
   name          = "media-ds"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
-
-# data "vsphere_folder" "cattles" {
-#   path = "cattle"
-# }
-
-# data "vsphere_folder" "windows" {
-#   path = "${data.vsphere_folder.cattles.path}/windows"
-# }
-
-# data "vsphere_folder" "linux" {
-#   path = "${data.vsphere_folder.cattles.path}/linux"
-# }
-
-# data "vsphere_folder" "docker_windows" {
-#   path = "${data.vsphere_folder.cattles.path}/${data.vsphere_folder.windows.path}/docker"
-# }
-
-# data "vsphere_folder" "docker_linux" {
-#   path = "${data.vsphere_folder.cattles.path}/${data.vsphere_folder.linux.path}/docker"
-# }
-
-# data "vsphere_folder" "gaming" {
-#   path = "${data.vsphere_folder.cattles.path}/${data.vsphere_folder.windows.path}/gaming"
-# }
-
-# data "vsphere_folder" "kubernetes" {
-#   path = "${data.vsphere_folder.cattles.path}/${data.vsphere_folder.linux.path}/kubernetes"
-# }
-
-# data "vsphere_folder" "dev" {
-#   path = "${data.vsphere_folder.cattles.path}/${data.vsphere_folder.linux.path}/dev"
-# }
-
-# data "vsphere_folder" "media" {
-#   path = "${data.vsphere_folder.cattles.path}/${data.vsphere_folder.linux.path}/media"
-# }
