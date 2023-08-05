accd310c023595e97436d355a401253f137e63ae
Bryan Jones <unix.chkpwd@gmail.com>
2023-08-05 12:22:01 -0400

    add(adguard): new dns entries separated into files

--- terraform/adguard-home/dns_rewrites.tf
+++ terraform/adguard-home/dns_rewrites.tf
@@ -10,3 +10,29 @@ resource "adguard_rewrite" "infra" {
   answer = each.value.answer
 
 }
+
+# manage a DNS rewrite rule for kubes
+resource "adguard_rewrite" "kubes" {
+  # Iterate over the records
+  for_each    = {
+    for record in jsondecode(file("${path.root}/template/kubes.json")) : record.domain => record
+  }
+
+  # Mapped values in json
+  domain = each.value.domain
+  answer = each.value.answer
+
+}
+
+# manage a DNS rewrite rule for traefik
+resource "adguard_rewrite" "traefik" {
+  # Iterate over the records
+  for_each    = {
+    for record in jsondecode(file("${path.root}/template/traefik.json")) : record.domain => record
+  }
+
+  # Mapped values in json
+  domain = each.value.domain
+  answer = each.value.answer
+
+}
