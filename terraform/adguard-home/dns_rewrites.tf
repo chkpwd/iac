d050be5290b63563a6840495380e979e1c2cfce0
Bryan Jones <unix.chkpwd@gmail.com>
2023-08-05 12:16:50 -0400

    add(adguard): set infra rewrite file

--- terraform/adguard-home/dns_rewrites.tf
+++ terraform/adguard-home/dns_rewrites.tf
@@ -2,7 +2,7 @@
 resource "adguard_rewrite" "infra" {
   # Iterate over the records
   for_each    = {
-    for record in jsondecode(file("${path.root}/template/rewrites.json")) : record.domain => record
+    for record in jsondecode(file("${path.root}/template/infra.json")) : record.domain => record
   }
 
   # Mapped values in json
