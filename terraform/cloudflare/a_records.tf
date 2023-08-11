b33401ee1eeac3ff119d39aca03ddd92091da6f0
Bryan Jones <unix.chkpwd@gmail.com>
2023-08-11 00:15:56 -0400

    fix(cloudflare): wrong output redirect on record

--- terraform/cloudflare/a_records.tf
+++ terraform/cloudflare/a_records.tf
@@ -30,6 +30,6 @@ resource "cloudflare_record" "couchdb" {
   proxied = true
   ttl     = 1
   type    = "A"
-  value   = data.tfe_outputs.oci.values.ct-02-x86_public_ip
+  value   = data.tfe_outputs.oci.values.ct-01-x86_public_ip
   zone_id = data.sops_file.cloudflare-secrets.data["cloudflare_zone_id"]
 }
