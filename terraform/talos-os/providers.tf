b7053370b89e464ce5bbb13671a32b83dea3bae1
Bryan Jones <unix.chkpwd@gmail.com>
2023-08-14 12:48:35 -0400

    deploy: talos-os tf root module

---
+++ terraform/talos-os/providers.tf
@@ -0,0 +1,40 @@
+# see https://github.com/hashicorp/terraform
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = "2.3.1"
+    }
+    talos = {
+      source  = "siderolabs/talos"
+      version = "0.1.1"
+    }
+    sops = {
+      source  = "carlpett/sops"
+      version = "0.7.2"
+    }
+  }
+}
+
+data "sops_file" "talos-secrets" {
+  source_file = "../terraform.sops.yaml"
+}
+
+provider "talos" {}
+
+# Configure the vSphere Provider
+provider "vsphere" {
+  vsphere_server = var.vsphere_server
+  user = data.sops_file.talos-secrets.data["vsphere_user"]
+  password = data.sops_file.talos-secrets.data["vsphere_password"]
+
+  allow_unverified_ssl = var.vsphere_unverified_ssl
+}
---
+++ terraform/talos-os/variables.tf
@@ -0,0 +1,63 @@
+variable "vsphere_server" {
+  description = "vCenter server FQDN or IP"
+  default = "ronin.local.chkpwd.com"
+  type        = string
+}
+
+variable "vsphere_unverified_ssl" {
+  default = "true"
+}
+
+variable "vsphere_datacenter" {
+  description = "vSphere datacenter"
+  default = "The Outlands"
+}
+
+variable "vsphere_compute_cluster" {
+  description = "A name to provide for the Talos cluster"
+  default     = "chkpwd"
+  type        = string
+}
+
+variable "vsphere_datastore" {
+  default = "nvme-30A"
+}
+
+variable "vsphere_network" {
+  default = "Lab"
+}
+
+variable "vsphere_folder" {
+  default = "kubernetes"
+}
+
+variable "vsphere_talos_template" {
+  default = "templates/talos-1.3.5-amd64"
+}
+
+variable "prefix" {
+  default = "tf-talos"
+}
+
+variable "controller_count" {
+  type    = number
+  default = 1
+  validation {
+    condition     = var.controller_count >= 1
+    error_message = "Must be 1 or more."
+  }
+}
+
+variable "worker_count" {
+  type    = number
+  default = 1
+  validation {
+    condition     = var.worker_count >= 1
+    error_message = "Must be 1 or more."
+  }
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "example"
+}
