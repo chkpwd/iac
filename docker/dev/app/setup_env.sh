e2a8eadc1f60e6008f9a4a8c125c84e46621af8d
Bryan Jones <unix.chkpwd@gmail.com>
2023-09-13 10:36:32 -0400

    add(dev,chezmoi): added more checks and conditions to var

--- docker/dev/app/setup_env.sh
+++ docker/dev/app/setup_env.sh
@@ -1,4 +1,4 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
 
 show_help() {
   cat << EOF
@@ -16,8 +16,18 @@ or
 Options:
   -h      Display this help and exit
 
+Environment Variables:
+  BW_API_KEY          Your Bitwarden API key, if you have one.
+  BW_EMAIL_ADDRESS    Your Bitwarden email address, used for login.
+  BW_PASSWORD         Your Bitwarden password.
+  GIT_USER            Your Git username, for Chezmoi initialization.
+  PULL_REPOS          If set to "yes", pulls all git repos. If set to "no", skips this step.
+
 EOF
 }
+
+# Chezmoi conf file
+CHEZMOI_CONF=~/.config/chezmoi/chezmoi.yaml
 
 # Check for '-h' or '--help' arguments
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
@@ -26,12 +36,13 @@ if [[ "$1" == "-h" || "$1" == "--help" ]]; then
 fi
 
 # Login to Bitwarden and set the BW_SESSION environment variable
-export BW_SESSION=$(/usr/bin/bw login $BW_EMAIL_ADDRESS $BW_PASSWORD --raw)
-
-# Check if the login was successful
-if [ -z "$BW_SESSION" ]; then
-    echo "Failed to login to Bitwarden."
-    exit 1
+if [[ -z "$BWS_ACCESS_SESSION" || yq -r '.data.accessToken' $CHEZMOI_CONF ]]; then
+   # https://bitwarden.com/help/personal-api-key/
+   if [[ "$BW_API_KEY" ]]; then
+      export BW_SESSION=$(/usr/bin/bw login --apikey $BW_API --raw)
+   else
+      export BW_SESSION=$(/usr/bin/bw login $BW_EMAIL_ADDRESS $BW_PASSWORD --raw)
+   fi
 fi
 
 # Set working directory
@@ -40,9 +51,13 @@ cd "$HOME" || return
 # Init Chezmoi
 sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply $GIT_USER
 
-if [ -z "$PULL_REPOS" = "no" ]; then
+if [ "$PULL_REPOS" = "no" ]; then
+    echo "Skipping pulling git repos."
+elif [ "$PULL_REPOS" = "yes" ]; then
     # Pull all git repos
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/chkpwd/scripts/main/Bash/git_pull_repos.sh)" -- $GIT_USER
+else
+    echo "PULL_REPOS not set to 'yes' or 'no'. Ignoring."
 fi
 
 # Source zsh
