207efe0915205c8eac6da74e90a06b765e7f01e2
Bryan Jones <unix.chkpwd@gmail.com>
2023-09-20 10:57:29 +0000

    fix(script, bash): WORKDIR included oci in for loop

--- kubernetes/toolkit/scripts/add-all-helm-repos-to-helm.sh
+++ kubernetes/toolkit/scripts/add-all-helm-repos-to-helm.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Set working dir
-WORKDIR="/home/hyoga/code/iac/kubernetes/infra"
+WORKDIR="$HOME/code/iac/kubernetes/infra"
 
 # Change into kubernetes infra dir
 cd $WORKDIR
 
-for file in sources/helm-repos/*.yaml; do
+for file in sources/helm-repos/*.yaml sources/oci/*.yaml; do
     name=$(yq -r '.metadata.name' $file);
     url=$(yq -r '.spec.url' $file);
 
