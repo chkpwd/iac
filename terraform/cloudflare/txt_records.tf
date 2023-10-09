8422461604e61573e5a3c7833d34d37e6a94aebb
Bryan Jones <unix.chkpwd@gmail.com>
2023-10-09 20:14:46 +0000

    feat(cloudflare): setup email routing

--- terraform/cloudflare/txt_records.tf
+++ terraform/cloudflare/txt_records.tf
@@ -5,3 +5,13 @@ resource "cloudflare_record" "github_txt" {
   value   = data.sops_file.cloudflare-secrets.data["git_txt_record_value"]
   zone_id = data.sops_file.cloudflare-secrets.data["cloudflare_zone_id"]
 }
+
+resource "cloudflare_record" "cloudflare_email_txt" {
+  name    = "@"
+  ttl     = 1
+  type    = "TXT"
+  value   = "v=spf1 include:_spf.mx.cloudflare.net ~all"
+  proxied  = false
+  comment  = "Terraform - SPF - email"
+  zone_id = data.sops_file.cloudflare-secrets.data["cloudflare_zone_id"]
+}
