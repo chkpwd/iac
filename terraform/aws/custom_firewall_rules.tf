a1b04d506bf4aa11bc0493cd14535ac63fbf5e2a
Bryan Jones <unix.chkpwd@gmail.com>
2023-10-24 13:16:54 +0000

    feat(cloud,aws): add ingress and egress rules

--- terraform/aws/custom_firewall_rules.tf
+++ terraform/aws/custom_firewall_rules.tf
@@ -1,12 +1,26 @@
 resource "aws_security_group" "secondary" {
   name        = "app-rules-group"
   description = "Custom security rules for apps"
-  vpc_id      = aws_vpc.main.id 
+  vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
