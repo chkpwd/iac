36e5d84e426c7e50365df413226b47f1aa80499a
Bryan Jones <unix.chkpwd@gmail.com>
2023-10-29 16:08:54 +0000

    infra: updated dns rules

--- terraform/adguard-home/dns_rewrites.tf
+++ terraform/adguard-home/dns_rewrites.tf
@@ -24,8 +24,19 @@ resource "adguard_rewrite" "kubes" {
 
 }
 
-# manage a DNS rewrite rule for traefik
-resource "adguard_rewrite" "ingress-nginx" {
+resource "adguard_rewrite" "int-ingress-nginx" {
+  # Iterate over the records
+  for_each    = {
+    for record in jsondecode(file("${path.root}/template/ingress-internal-svc.json")) : record.domain => record
+  }
+
+  # Mapped values in json
+  domain = each.value.domain
+  answer = each.value.answer
+
+}
+
+resource "adguard_rewrite" "ext-ingress-nginx" {
   # Iterate over the records
   for_each    = {
     for record in jsondecode(file("${path.root}/template/ingress-external-svc.json")) : record.domain => record
