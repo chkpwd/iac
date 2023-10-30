76f83732b2a5261a2fa9a812309de064d11b8c85
Bryan Jones <unix.chkpwd@gmail.com>
2023-10-30 19:05:56 +0000

    feat: added BWS token

--- docker/dev/app/setup_env.sh
+++ docker/dev/app/setup_env.sh
@@ -17,6 +17,7 @@ Options:
   -h      Display this help and exit
 
 Environment Variables:
+  BWS_ACCESS_TOKEN    Your Bitwarden access token. Link: https://bitwarden.com/help/access-tokens/
   BW_API_KEY          Your Bitwarden API key, if you have one.
   BW_EMAIL_ADDRESS    Your Bitwarden email address, used for login.
   BW_PASSWORD         Your Bitwarden password.
@@ -27,7 +28,8 @@ EOF
 }
 
 # Chezmoi conf file
-CHEZMOI_CONF=~/.config/chezmoi/chezmoi.yaml
+CHEZMOI_CONF_DIR=~/.config/chezmoi
+CHEZMOI_CONF="${CHEZMOI_CONF_DIR}/chezmoi.yaml"
 
 # Check for '-h' or '--help' arguments
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
@@ -35,14 +37,29 @@ if [[ "$1" == "-h" || "$1" == "--help" ]]; then
     exit 0
 fi
 
+# Function to set up Chezmoi configuration
+setup_chezmoi_conf() {
+  # Ensure the Chezmoi configuration directory exists
+  mkdir -p "$CHEZMOI_CONF_DIR"
+  
+  # Prompt user for access token
+  read -rp "Enter your access token: " access_token
+  
+  # Create Chezmoi configuration file with access token
+  echo "data:
+  accessToken: $access_token" > "$CHEZMOI_CONF"
+}
+
 # Login to Bitwarden and set the BW_SESSION environment variable
-if [[ -z "$BWS_ACCESS_SESSION" ]] && [[ -z $(yq '(.[] | select(has("accessToken")).accessToken)' "$CHEZMOI_CONF") ]]; then
-   # https://bitwarden.com/help/personal-api-key/
-   if [[ "$BW_API_KEY" ]]; then
-      export BW_SESSION=$(/usr/bin/bw login --apikey $BW_CLIENTID $BW_CLIENTSECRET --raw)
-   else
-      export BW_SESSION=$(/usr/bin/bw login $BW_EMAIL_ADDRESS $BW_PASSWORD --raw)
-   fi
+if [[ -z "$BWS_ACCESS_TOKEN" ]] || [[ -z $(yq '(.[] | select(has("accessToken")).accessToken)' "$CHEZMOI_CONF") ]]; then
+  # https://bitwarden.com/help/personal-api-key/
+  if [[ -n "$BW_API_KEY" ]]; then
+    export BW_SESSION=$(/usr/bin/bw login --apikey $BW_CLIENTID $BW_CLIENTSECRET --raw)
+  else
+    setup_chezmoi_conf
+    access_token=$(yq -r '.data.accessToken' "$CHEZMOI_CONF")
+    export BW_SESSION=$(/usr/bin/bw login --apikey $access_token --raw)
+  fi
 fi
 
 # Set working directory
