f0b52f293b60b2b492c5b906410cf00b8966c02a
Bryan Jones <unix.chkpwd@gmail.com>
2023-11-12 00:10:17 +0000

    feat(infra): change common user

--- terraform/ansible/k3s.tf
+++ terraform/ansible/k3s.tf
@@ -5,10 +5,10 @@
 # resource "ansible_playbook" "playbook" {
 #   groups     = ["docker_hosts"]
 #   name       = "media-srv-01"
-#   playbook   = "/home/hyoga/code/iac/ansible/playbooks/get_macaddress.yaml"
+#   playbook   = "/home/chkpwd/code/iac/ansible/playbooks/get_macaddress.yaml"
 #   #replayable = true
 #   #diff_mode  = true
-#   vault_password_file = "/home/hyoga/code/iac/ansible/vault-password"
+#   vault_password_file = "/home/chkpwd/code/iac/ansible/vault-password"
 # } 
 
 resource "ansible_group" "group" {
