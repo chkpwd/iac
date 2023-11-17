6b833a512381a9512e1c59db19cc38e1a36a50bb
Bryan Jones <unix.chkpwd@gmail.com>
2023-11-17 14:40:43 +0000

    add(terraform,authentik): plex source

--- terraform/authentik/main.tf
+++ terraform/authentik/main.tf
@@ -14,3 +14,14 @@ resource "authentik_outpost" "main" {
   ]
   service_connection = authentik_service_connection_kubernetes.main.id
 }
+
+resource "authentik_source_plex" "main" {
+  enabled = true
+  name                = "plex"
+  slug                = "plex"
+  authentication_flow = data.authentik_flow.default-authorization-flow.id
+  enrollment_flow     = data.authentik_flow.default-authorization-flow.id
+  client_id           = data.sops_file.authentik-secrets.data["main_plex_client_id"]
+  plex_token          = data.sops_file.authentik-secrets.data["main_plex_token"]
+  allow_friends = true
+}
