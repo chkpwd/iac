51e96f75fe3cbdfbf7f130c4cbdb058acc7390b5
Bryan Jones <unix.chkpwd@gmail.com>
2023-11-29 14:49:35 +0000

    feat(terraform,oci): changed vm names

--- terraform/oci/main.tf
+++ terraform/oci/main.tf
@@ -1,43 +1,43 @@
-module "ct-01-aarch64" {
+# module "ct-01-aarch64" {
+#   source                         = "../_modules/oci_vm"
+#   count                          = 4
+#   instance_spec                  = {
+#     name                         = "ct-01-aarch64"
+#     cpus                         = 1
+#     memory_gb                    = 2
+#     disk_size                    = 50
+#     shape                        = "VM.Standard.A1.Flex" 
+#     image_id                     = "ocid1.image.oc1.iad.aaaaaaaa7bvpvpgorsj2ciivlrxd4acxcjpbuhfv26pld3qnhjav5hpamm5q"
+#     network                      = {
+#       subnet_id                  = "${data.sops_file.oci-secrets.data["oci_subnet_ocid"]}"
+#       vnic_label                 = "Primaryvnic"
+#       hostname                   = "ct-01-aarch64"
+#       assign_public_ip           = true
+#       assign_private_dns_record  = true
+#     }
+#     ssh_authorized_keys          = var.main_pub_key
+#   }
+
+#   ssh_allowed_ips                = [
+#     {
+#       description                = "Public IP for homelab"
+#       ip                         = "${data.sops_file.oci-secrets.data["public_address"]}/32"
+#     }
+#   ]
+#   oci_availability_domain_number = var.availability_domain
+# }
+
+module "ct-01-oci" {
   source                         = "../_modules/oci_vm"
-  count                          = 4
   instance_spec                  = {
-    name                         = "ct-01-aarch64"
+    name                         = "ct-01-oci"
     cpus                         = 1
-    memory_gb                    = 2
-    disk_size                    = 50
-    shape                        = "VM.Standard.A1.Flex" 
-    image_id                     = "ocid1.image.oc1.iad.aaaaaaaa7bvpvpgorsj2ciivlrxd4acxcjpbuhfv26pld3qnhjav5hpamm5q"
-    network                      = {
-      subnet_id                  = "${data.sops_file.oci-secrets.data["oci_subnet_ocid"]}"
-      vnic_label                 = "Primaryvnic"
-      hostname                   = "ct-01-aarch64"
-      assign_public_ip           = true
-      assign_private_dns_record  = true
-    }
-    ssh_authorized_keys          = var.main_pub_key
-  }
-
-  ssh_allowed_ips                = [
-    {
-      description                = "Public IP for homelab"
-      ip                         = "${data.sops_file.oci-secrets.data["public_address"]}/32"
-    }
-  ]
-  oci_availability_domain_number = var.availability_domain
-}
-
-module "ct-01-x86" {
-  source                         = "../_modules/oci_vm"
-  instance_spec                  = {
-    name                         = "ct-01-x86"
-    cpus                         = 2
-    memory_gb                    = 2
+    memory_gb                    = 1
     disk_size                    = 50
     shape                        = "VM.Standard.E2.1.Micro" 
-    image_id                     = "ocid1.image.oc1.iad.aaaaaaaaau2eo3mjbgtmjvocmvx5xbhcmj2ay3mvowdzffxhdiql5gnhxjqa"
+    image_id                     = "ocid1.image.oc1.iad.aaaaaaaacwawwkkpizhbin2dmcrelcqjhqgji6nuloizwl6xix4dwezgrfyq"
     network                      = {
-      subnet_id                  = "${data.sops_file.oci-secrets.data["oci_subnet_ocid"]}"
+      subnet_id                  = data.sops_file.oci-secrets.data["oci_subnet_ocid"]
       vnic_label                 = "Primaryvnic"
       hostname                   = "ct-01-x86"
       assign_public_ip           = true
@@ -52,20 +52,20 @@ module "ct-01-x86" {
       ip                         = "${data.sops_file.oci-secrets.data["public_address"]}/32"
     }
   ]
-  oci_availability_domain_number = 2
+  oci_availability_domain_number = 1
 }
 
-module "ct-02-x86" {
+module "ct-02-oci" {
   source                         = "../_modules/oci_vm"
   instance_spec                  = {
-    name                         = "ct-02-x86"
-    cpus                         = 2
-    memory_gb                    = 2
+    name                         = "ct-02-oci"
+    cpus                         = 1
+    memory_gb                    = 1
     disk_size                    = 50
     shape                        = "VM.Standard.E2.1.Micro" 
-    image_id                     = "ocid1.image.oc1.iad.aaaaaaaaau2eo3mjbgtmjvocmvx5xbhcmj2ay3mvowdzffxhdiql5gnhxjqa"
+    image_id                     = "ocid1.image.oc1.iad.aaaaaaaacwawwkkpizhbin2dmcrelcqjhqgji6nuloizwl6xix4dwezgrfyq"
     network                      = {
-      subnet_id                  = "${data.sops_file.oci-secrets.data["oci_subnet_ocid"]}"
+      subnet_id                  = data.sops_file.oci-secrets.data["oci_subnet_ocid"]
       vnic_label                 = "Primaryvnic"
       hostname                   = "ct-02-x86"
       assign_public_ip           = true
@@ -80,5 +80,5 @@ module "ct-02-x86" {
       ip                         = "${data.sops_file.oci-secrets.data["public_address"]}/32"
     }
   ]
-  oci_availability_domain_number = 2
+  oci_availability_domain_number = 1
 }
--- terraform/oci/outputs.tf
+++ terraform/oci/outputs.tf
@@ -1,11 +1,11 @@
-output "ct-01-x86_public_ip" {
-    value = module.ct-01-x86.public_ip
+output "ct-01-oci_public_ip" {
+  value = module.ct-01-oci.public_ip
 }
 
-output "ct-02-x86_public_ip" {
-    value = module.ct-02-x86.public_ip
+output "ct-02-oci_public_ip" {
+  value = module.ct-02-oci.public_ip
 }
 
-output "ct-02-x86_nsg_id" {
-    value = module.ct-02-x86.nsg_id
+output "ct-02-oci_nsg_id" {
+  value = module.ct-02-oci.nsg_id
 }
