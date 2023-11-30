f0e974640cfb79c2ccdc2e1d3724b64117bd8174
Bryan Jones <unix.chkpwd@gmail.com>
2023-11-30 19:07:23 +0000

    add windows VMs

--- terraform/vsphere/main.tf
+++ terraform/vsphere/main.tf
@@ -47,32 +47,11 @@ module "media-srv-01" {
   }
 }
 
-module "homeassistant" {
-  source                    = "../_modules/vsphere_vm"
-  vm_name                   = "home-assistant"
-  vm_template               = "deb-12-template"
-  network_spec = {
-    network_id              = "LAN"
-  }
-  spec = {
-    tags                    = [ vsphere_tag.cattle.id, vsphere_tag.linux.id, vsphere_tag.docker.id ]
-    folder                  = vsphere_folder.personal_linux.path
-    cpu                     = 4
-    memory                  = 1024 * 4
-    disk_size               = 16
-    additional_disks = [ 
-      {
-        size                = 25
-      } 
-    ]
-  }
-}
-
 module "win-srv-2022" {
   source                    = "../_modules/vsphere_vm"
   count                     = 1
   vm_name                   = "win-srv-2022"
-  vm_template               = "WSrv22-DE-Temp"
+  vm_template               = "WSRV22-DE-Temp"
   network_spec = {
     network_id              = "IoT"
   }
@@ -112,27 +91,27 @@ module "hosting-srv-01" {
   }
 }
 
-# module "win10-gaming-01" {
-#   source                    = "../_modules/vsphere_vm"
-#   count                     = 1
-#   vm_name                   = "win10-gaming-01"
-#   vm_template               = "W10-22H2-Temp"
-#   network_spec = {
-#     network_id              = "Lab"
-#   }
-#   spec = {
-#     tags                    = [ vsphere_tag.cattle.id, vsphere_tag.windows.id ]
-#     folder                  = vsphere_folder.gaming_windows.path
-#     cpu                     = 8
-#     memory                  = 1024 * 16
-#     memory_reservation      = true
-#     pci_device              = [ data.vsphere_host_pci_device.nvidia_1050ti.id ]
-#     disk_size               = 48
-#     scsi_type               = "lsilogic-sas"
-#     additional_disks = [
-#       {
-#         size                = 100
-#       }
-#     ]
-#   }
-# }
+module "win10-gaming-01" {
+  source                    = "../_modules/vsphere_vm"
+  count                     = 1
+  vm_name                   = "win10-gaming-01"
+  vm_template               = "W10-22H2-Temp"
+  network_spec = {
+    network_id              = "Lab"
+  }
+  spec = {
+    tags                    = [ vsphere_tag.cattle.id, vsphere_tag.windows.id ]
+    folder                  = vsphere_folder.gaming_windows.path
+    cpu                     = 8
+    memory                  = 1024 * 16
+    memory_reservation      = true
+    pci_device              = [ data.vsphere_host_pci_device.nvidia_1050ti.id ]
+    disk_size               = 48
+    scsi_type               = "lsilogic-sas"
+    additional_disks = [
+      {
+        size                = 100
+      }
+    ]
+  }
+}
