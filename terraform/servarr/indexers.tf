f20ec95efceebf0970b2f436cbc9d2bde008a7c5
Bryan Jones <unix.chkpwd@gmail.com>
2023-12-31 14:28:52 -0500

    fix(prowlarr): add indexers

--- terraform/servarr/indexers.tf
+++ terraform/servarr/indexers.tf
@@ -3,6 +3,7 @@ resource "prowlarr_indexer" "usenet_nzbgeek" {
   name            = "NZBGeek"
   implementation  = "Newznab"
   config_contract = "NewznabSettings"
+  app_profile_id  = 1
   protocol        = "usenet"
   priority        = 1
   tags            = []
@@ -27,19 +28,19 @@ resource "prowlarr_indexer" "usenet_nzbgeek" {
   ]
 }
 
-resource "prowlarr_indexer" "torrent_animetosho" {
-  enable          = true
-  name            = "AnimeTosho"
-  implementation  = "Torznab"
-  config_contract = "TorznabSettings"
-  protocol        = "torrent"
-  priority        = 25
+resource "prowlarr_indexer" "usenet_drunkenslug" {
+  enable          = false
+  name            = "DrunkenSlug"
+  implementation  = "Newznab"
+  config_contract = "NewznabSettings"
+  app_profile_id  = 1
+  protocol        = "usenet"
   tags            = []
 
   fields = [
     {
       name: "baseUrl",
-      text_value: "https://feed.animetosho.org"
+      text_value: "https://drunkenslug.com"
     },
     {
       name: "apiPath"
@@ -47,43 +48,15 @@ resource "prowlarr_indexer" "torrent_animetosho" {
     },
     {
       name: "apiKey"
-      text_value: ""
+      text_value: "${data.sops_file.servarr-secrets.data["drunkenslug_api_key"]}"
     },
     {
-      name: "vipExpiration"
-      text_value: ""
+      name: "grabLimit"
+      number_value: "20"
+    },
+    {
+      name: "queryLimit"
+      number_value: "20"
     }
   ]
 }
-
-# resource "prowlarr_indexer" "usenet_drunkenslug" {
-#   enable          = true
-#   name            = "DrunkenSlug"
-#   implementation  = "Newznab"
-#   config_contract = "NewznabSettings"
-#   protocol        = "usenet"
-#   tags            = []
-
-#   fields = [
-#     {
-#       name: "baseUrl",
-#       text_value: "https://drunkenslug.com"
-#     },
-#     {
-#       name: "apiPath"
-#       text_value: "/api"
-#     },
-#     {
-#       name: "apiKey"
-#       text_value: "e983f92c8dfe05ac4de52b7cec85dfb2"
-#     },
-#     {
-#       name: "grabLimit"
-#       number_value: "20"
-#     },
-#     {
-#       name: "queryLimit"
-#       number_value: "20"
-#     }
-#   ]
-# }
