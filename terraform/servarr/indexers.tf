84d390e81310450fa8106febb3891c328d38eb3f
Bryan Jones <unix.chkpwd@gmail.com>
2024-02-07 19:48:57 +0000

    removed indexer

--- terraform/servarr/indexers.tf
+++ terraform/servarr/indexers.tf
@@ -27,36 +27,3 @@ resource "prowlarr_indexer" "usenet_nzbgeek" {
     }
   ]
 }
-
-resource "prowlarr_indexer" "usenet_drunkenslug" {
-  enable          = false
-  name            = "DrunkenSlug"
-  implementation  = "Newznab"
-  config_contract = "NewznabSettings"
-  app_profile_id  = 1
-  protocol        = "usenet"
-  tags            = []
-
-  fields = [
-    {
-      name: "baseUrl",
-      text_value: "https://drunkenslug.com"
-    },
-    {
-      name: "apiPath"
-      text_value: "/api"
-    },
-    {
-      name: "apiKey"
-      text_value: "${data.sops_file.servarr-secrets.data["drunkenslug_api_key"]}"
-    },
-    {
-      name: "grabLimit"
-      number_value: "20"
-    },
-    {
-      name: "queryLimit"
-      number_value: "20"
-    }
-  ]
-}
