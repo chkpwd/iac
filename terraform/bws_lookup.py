8ad94aeeb3ee7e897613727a4a3ab1561c091b4a
Bryan Jones <unix.chkpwd@gmail.com>
2024-03-11 12:00:28 -0400

    chore(python): add logging to bws_lookup

--- terraform/bws_lookup.py
+++ terraform/bws_lookup.py
@@ -1,10 +1,21 @@
 import json
 import sys
+import logging
 import os
 import requests
 
+class InvalidToken(Exception):
+    pass
+
+logging.basicConfig(filename='tf.log', encoding='utf-8', level=logging.DEBUG)
+
 access_token = os.environ.get("BWS_ACCESS_TOKEN")
+if not access_token:
+    raise InvalidToken("Token is not set")
+
 key_name = json.load(sys.stdin)["key"].split(",")
+
+logging.info(key_name)
 results = []
 
 for key in key_name:
@@ -14,9 +25,16 @@ for key in key_name:
         timeout=10,
     ).json()
 
-    results.append(bws_response['value'])
+    logging.debug(bws_response)
+
+    try:
+        results.append(bws_response['value'])
+    except KeyError:
+        raise InvalidToken("Token is invalid or does not have permissions to read value")
 
 flat = {}
 
 for name, value in zip(key_name, results):
     flat.update({f"{name}_{dict_key}":dict_value for dict_key, dict_value in value.items()})
+
+print(json.dumps(flat))
