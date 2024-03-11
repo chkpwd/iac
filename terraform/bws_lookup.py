469b448d3ddfcb7349e3549e041d4d132e6b2950
Bryan Jones <unix.chkpwd@gmail.com>
2024-03-10 22:27:21 -0400

    fix: list flattening via tuple unpacking

--- terraform/bws_lookup.py
+++ terraform/bws_lookup.py
@@ -3,35 +3,20 @@ import sys
 import os
 import requests
 
-def flatten_json(y):
-    out = {}
-
-    def flatten(x, name=''):
-        if isinstance(x, dict):
-            for a in x:
-                flatten(x[a], name + a)
-        elif isinstance(x, list):
-            for a in x:
-                flatten(a, name)
-        else:
-            out[name] = x
-
-    flatten(y)
-    return out
-
 access_token = os.environ.get("BWS_ACCESS_TOKEN")
 key_name = json.load(sys.stdin)["key"].split(",")
-
 results = []
 
 for key in key_name:
     bws_response = requests.get(
-        f"http://mgmt-srv-01:5000/key/{key}",
+        f"http://172.16.16.4:5000/key/{key}",
         headers={"Authorization": f"Bearer {access_token}"},
         timeout=10,
     ).json()
 
     results.append(bws_response['value'])
 
-flat = flatten_json(results)
-print(json.dumps(flat))
+flat = {}
+
+for name, value in zip(key_name, results):
+    flat.update({f"{name}_{dict_key}":dict_value for dict_key, dict_value in value.items()})
