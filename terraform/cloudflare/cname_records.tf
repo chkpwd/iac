16ef45f0f51be0f64fecb34ac4fe01a380fc8e0c
Bryan Jones <unix.chkpwd@gmail.com>
2024-04-05 11:59:26 -0400

    feat(cloudflare): add mkdocs cname

--- terraform/cloudflare/cname_records.tf
+++ terraform/cloudflare/cname_records.tf
@@ -6,3 +6,12 @@ resource "cloudflare_record" "blog" {
   value   = "chkpwd.github.io"
   zone_id = data.external.bws_lookup.result["cloudflare-dns-secrets_zone_id"]
 }
+
+resource "cloudflare_record" "docs" {
+  name    = "docs"
+  proxied = true
+  ttl     = 1
+  type    = "CNAME"
+  value   = "chkpwd.github.io"
+  zone_id = data.external.bws_lookup.result["cloudflare-dns-secrets_zone_id"]
+}
