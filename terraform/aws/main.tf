01df0a1bb89a1da0f257254c7b2b6ae02b55ef73
Bryan Jones <unix.chkpwd@gmail.com>
2024-05-10 09:58:27 -0400

    formatted

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,11 +1,11 @@
 module "ct-01-ec2" {
-  source                         = "../_modules/aws_vm"
-  instance_spec                  = {
-    name                         = "ct-01-ec2"
-    ami                          = "ami-06db4d78cb1d3bbf9" # Debian 12 amd64
-    type                         = "t2.micro"
-    key_name                     = aws_key_pair.main.key_name
+  source = "../_modules/aws_vm"
+  instance_spec = {
+    name     = "ct-01-ec2"
+    ami      = "ami-06db4d78cb1d3bbf9" # Debian 12 amd64
+    type     = "t2.micro"
+    key_name = aws_key_pair.main.key_name
   }
-  subnet_id                      = aws_subnet.main.id
-  vpc_security_groups            = [ aws_security_group.main.id, aws_security_group.secondary.id ]
+  subnet_id           = aws_subnet.main.id
+  vpc_security_groups = [aws_security_group.main.id, aws_security_group.secondary.id]
 }
