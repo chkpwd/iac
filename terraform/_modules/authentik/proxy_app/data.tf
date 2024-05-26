16704c582e660e5bae9f2737e56cc40774b8cb1f
Bryan Jones <unix.chkpwd@gmail.com>
2024-05-26 17:53:41 -0400

    feat(authentik): add authentication flow data

--- terraform/_modules/authentik/proxy_app/data.tf
+++ terraform/_modules/authentik/proxy_app/data.tf
@@ -1,3 +1,7 @@
 data "authentik_flow" "default-authorization-flow" {
   slug = "default-provider-authorization-implicit-consent"
 }
+
+data "authentik_flow" "default-authentication-flow" {
+  slug = "default-authentication-flow"
+}
