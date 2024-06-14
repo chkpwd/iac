95b5a020a6266d78c584c3116679bde98f16b3d3
Bryan Jones <unix.chkpwd@gmail.com>
2024-06-14 19:12:05 -0400

    chore: fix opnsense interfaces

--- terraform/opnsense/interfaces.tf
+++ terraform/opnsense/interfaces.tf
@@ -1,15 +1,15 @@
 resource "opnsense_interfaces_vlan" "vlan01" {
-  device = "vlan01"
-  parent = "em0"
-  tag = 10
-  priority = 0
+  device      = "vlan01"
+  parent      = "em0"
+  tag         = 10
+  priority    = 0
   description = "IOT"
 }
 
 resource "opnsense_interfaces_vlan" "vlan02" {
-  device = "vlan02"
-  parent = "em0"
-  tag = 20
-  priority = 0
+  device      = "vlan02"
+  parent      = "em0"
+  tag         = 20
+  priority    = 0
   description = "LAB"
 }
