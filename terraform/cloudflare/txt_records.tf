453c20139cc2d29f64715c3abf8b95111123e812
Bryan Jones <unix.chkpwd@gmail.com>
2024-11-03 16:35:30 -0500

    fix(cloudflare): add quotes surrounding records

--- terraform/cloudflare/txt_records.tf
+++ terraform/cloudflare/txt_records.tf
@@ -2,7 +2,7 @@ resource "cloudflare_dns_record" "github_txt" {
   name    = "_github-challenge-chkpwd-org.chkpwd.com"
   ttl     = 1
   type    = "TXT"
-  value   = data.external.bws_lookup.result["cloud-github-secrets_git_txt_record"]
+  content = "\"${data.external.bws_lookup.result["cloud-github-secrets_git_txt_record"]}\""
   zone_id = data.external.bws_lookup.result["cloudflare-dns-secrets_zone_id"]
 }
 
@@ -10,7 +10,7 @@ resource "cloudflare_record" "cloudflare_email_txt" {
   name    = "@"
   ttl     = 1
   type    = "TXT"
-  value   = "v=spf1 include:_spf.mx.cloudflare.net ~all"
+  content = "\"v=spf1 include:_spf.mx.cloudflare.net ~all\""
   proxied = false
   comment = "Terraform - SPF - email"
   zone_id = data.external.bws_lookup.result["cloudflare-dns-secrets_zone_id"]
@@ -20,7 +20,7 @@ resource "cloudflare_record" "aws_instance_proxy_txt" {
   name    = "@"
   ttl     = 1
   type    = "TXT"
-  value   = data.external.bws_lookup.result["cloud-aws-proxy-secrets_swag_txt_record"]
+  content = "\"${data.external.bws_lookup.result["cloud-aws-proxy-secrets_swag_txt_record"]}\""
   proxied = false
   comment = "Terraform - AWS Instance Proxy - TXT Record"
   zone_id = data.external.bws_lookup.result["cloudflare-dns-secrets_zone_id"]
