d9521d1ecf40475887e9066f76ce10982b571399
Bryan Jones <unix.chkpwd@gmail.com>
2025-03-05 18:25:12 -0500

    feat(cloudflare): add bucket for postgres backups

---
+++ terraform/cloudflare/buckets.tf
@@ -0,0 +1,6 @@
+resource "cloudflare_r2_bucket" "pg-cluster" {
+  account_id    = data.external.bws_lookup.result["cloudflare-dns-secrets_account_id"]
+  name          = "pg-cluster-backups"
+  location      = "ENAM"
+  storage_class = "Standard"
+}
