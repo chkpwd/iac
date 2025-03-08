d0f3d27a04c8c6aa2dafbdbc8e859d63ecb9d750
Bryan Jones <unix.chkpwd@gmail.com>
2025-03-08 14:06:31 -0500

    removed minio config

--- terraform/_modules/minio/providers.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = "1.11.0"
-  required_providers {
-    minio = {
-      source  = "aminueza/minio"
-      version = "3.3.0"
-    }
-  }
-}
--- terraform/minio/directory.tf
+++
@@ -1,7 +0,0 @@
-resource "minio_iam_user" "main" {
-  name          = "restic_k8s"
-  force_destroy = true
-  tags = {
-    tag-key = "restic"
-  }
-}
