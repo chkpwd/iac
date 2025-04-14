6cdec91b680ba3780fe5de5235b91e261ed9a413
Bryan Jones <unix.chkpwd@gmail.com>
2025-04-14 07:51:11 -0400

    chore(immich): add additional comments to database commands

--- kubernetes/apps/immich/database-init-cm.sql
+++ kubernetes/apps/immich/database-init-cm.sql
@@ -2,8 +2,8 @@ CREATE DATABASE immich;
 \c immich
 BEGIN;
 ALTER DATABASE immich OWNER TO immich;
+CREATE EXTENSION IF NOT EXISTS earthdistance CASCADE; -- installs dependencies (i.e. cube)
 CREATE EXTENSION IF NOT EXISTS vectors;
-CREATE EXTENSION IF NOT EXISTS earthdistance CASCADE;
 ALTER DATABASE immich SET search_path TO "$user", public, vectors;
 ALTER SCHEMA vectors OWNER TO immich;
 COMMIT;
