03c950bc6c21aefab5d028426aa5328b3df469ea
Bryan Jones <unix.chkpwd@gmail.com>
2025-05-02 16:55:49 -0400

    misc

--- terraform/proxmox/outputs.tf
+++ terraform/proxmox/outputs.tf
@@ -1,4 +1,4 @@
-output "packer_token_value" {
-  value     = proxmox_virtual_environment_user_token.packer.value
-  sensitive = true
-}
+# output "packer_token_value" {
+#   value     = proxmox_virtual_environment_user_token.packer.value
+#   sensitive = true
+# }
