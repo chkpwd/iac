42f9dd7a3053a7f71b526dc97b15cd7528a15580
Bryan Jones <unix.chkpwd@gmail.com>
2025-05-02 16:55:38 -0400

    feat(terraform): add gpu mapping for titan x maxwell

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -5,71 +5,16 @@ resource "proxmox_virtual_environment_cluster_options" "options" {
   max_workers = 5
 }
 
-# module "test-vm-1" {
-#   source = "../_modules/proxmox_vm"
-#   machine = {
-#     name         = "testing-vm-1"
-#     id           = 100
-#     tags         = ["test1", "test2"]
-#     enable_agent = true
-#     bios         = "seabios"
-#   }
-
-#   spec = {
-#     cpu = {
-#       cores = 2
-#     }
-#     memory = {
-#       dedicated = 2048
-#     }
-#     disk = {
-#       size         = 32
-#       interface    = "scsi0"
-#       datastore_id = "prod-nvme"
-#       discard      = "on"
-#       iothread     = false # only compatible with virtio-scsi-single controller
-#       file_id      = proxmox_virtual_environment_download_file.ubuntu_noble_cloud_image.id
-#     }
-#     network = {
-#       bridge = "vmbr10"
-#     }
-#   }
-# }
-
-# module "test-vm-2" {
-#   source = "../_modules/proxmox_vm"
-#   machine = {
-#     name         = "testing-vm-2"
-#     id           = 102
-#     tags         = ["test1", "test2"]
-#     enable_agent = true
-#     bios         = "seabios"
-#   }
-
-#   spec = {
-#     cpu = {
-#       cores = 2
-#     }
-#     memory = {
-#       dedicated = 2048
-#     }
-#     disk = {
-#       size         = 32
-#       interface    = "scsi0"
-#       datastore_id = "prod-nvme"
-#       discard      = "on"
-#       iothread     = false # only compatible with virtio-scsi-single controller
-#       file_id      = proxmox_virtual_environment_download_file.ubuntu_noble_cloud_image.id
-#     }
-#     network = {
-#       bridge = "vmbr10"
-#     }
-
-#     # initialization = { # TODO: doesn't work
-#     #   user_account = {
-#     #     username = "user"
-#     #     password = "password"
-#     #   }
-#     # }
-#   }
-# }
+resource "proxmox_virtual_environment_hardware_mapping_pci" "titan-x" {
+  name = "titan-x"
+  map = [
+    {
+      id           = "10de:17c2"
+      iommu_group  = 47
+      node         = var.node
+      path         = "0000:44:00.0"
+      subsystem_id = "3842:2992"
+    },
+  ]
+  mediated_devices = false
+}
