fa6edb9fa57ea3995ec7ebe28bb91e878ab000ec
Bryan Jones <unix.chkpwd@gmail.com>
2025-05-08 11:53:23 -0400

    forgot

--- terraform/proxmox/snippets.tf
+++ terraform/proxmox/snippets.tf
@@ -7,7 +7,6 @@ resource "proxmox_virtual_environment_file" "common_cloud_init" {
     data = <<-EOF
     #cloud-config
     timezone: America/New_York
-    preserve_hostname: false
     ssh_import_id: ["gh:chkpwd"]
     users:
       - default
@@ -24,5 +23,35 @@ resource "proxmox_virtual_environment_file" "common_cloud_init" {
     EOF
 
     file_name = "user-data-cloud-config.yaml"
+  }
+}
+
+resource "proxmox_virtual_environment_file" "common_network" {
+  content_type = "snippets"
+  datastore_id = "nas"
+  node_name    = var.node
+
+  source_raw {
+    data = <<-EOF
+    #cloud-config
+    network:
+      version: 2
+      renderer: networkd
+      ethernets:
+        enp1s0:
+          mtu: 9000
+          addresses:
+            - 10.10.0.6/24
+          dhcp4: true
+          dhcp4-overrides:
+            use-dns: true
+            use-ntp: true
+            send-hostname: true
+            use-routes: false
+            use-domains: true
+          accept-ra: false
+    EOF
+
+    file_name = "network.yaml"
   }
 }
