e58590cc53cf484e84dce31053108cc15d749322
Bryan Jones <unix.chkpwd@gmail.com>
2025-06-09 19:24:51 -0400

    add kubernetes and helm providers

--- terraform/kasten/providers.tf
+++ terraform/kasten/providers.tf
@@ -9,13 +9,29 @@ terraform {
       source  = "hashicorp/external"
       version = "~> 2"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.0-pre2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
+}
+
+data "aws_eks_cluster" "eks" {
+  name = aws_eks_cluster.eks.name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.eks.name
 }
 
 data "external" "bws_lookup" {
   program = ["python3", "../bws_lookup.py"]
   query = {
-    key = "cloud-aws-secrets"
+    key = "cloud-aws-secrets,demo-kasten"
   }
 }
 
@@ -23,4 +39,17 @@ provider "aws" {
   region     = local.region
   access_key = data.external.bws_lookup.result["cloud-aws-secrets_aws_access_key_id"]
   secret_key = data.external.bws_lookup.result["cloud-aws-secrets_aws_secret_access_key"]
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "arn:aws:eks:us-east-1:970547363121:cluster/kasten-staging"
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
 }
