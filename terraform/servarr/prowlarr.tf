f24276ca90a8180518624a878eb8e85427dec428
Bryan Jones <unix.chkpwd@gmail.com>
2025-06-15 22:24:03 -0400

    chore(servarr): update services dns

--- terraform/servarr/prowlarr.tf
+++ terraform/servarr/prowlarr.tf
@@ -1,8 +1,8 @@
 resource "prowlarr_application_sonarr" "sonarr" {
   name                  = "sonarr"
   sync_level            = "fullSync"
-  base_url              = "http://sonarr.${var.cluster_media_domain}:${var.ports["sonarr"]}"
-  prowlarr_url          = "http://prowlarr.${var.cluster_media_domain}:${var.ports["prowlarr"]}"
+  base_url              = "http://sonarr-app.${var.cluster_media_domain}:${var.ports["sonarr"]}"
+  prowlarr_url          = "http://prowlarr-app.${var.cluster_media_domain}:${var.ports["prowlarr"]}"
   api_key               = data.external.bws_lookup.result["infra-media-secrets_sonarr_api_key"]
   sync_categories       = [5000, 5010, 5030]
   anime_sync_categories = [5070]
@@ -11,8 +11,8 @@ resource "prowlarr_application_sonarr" "sonarr" {
 resource "prowlarr_application_radarr" "radarr" {
   name            = "radarr"
   sync_level      = "fullSync"
-  base_url        = "http://radarr.${var.cluster_media_domain}:${var.ports["radarr"]}"
-  prowlarr_url    = "http://prowlarr.${var.cluster_media_domain}:${var.ports["prowlarr"]}"
+  base_url        = "http://radarr-app.${var.cluster_media_domain}:${var.ports["radarr"]}"
+  prowlarr_url    = "http://prowlarr-app.${var.cluster_media_domain}:${var.ports["prowlarr"]}"
   api_key         = data.external.bws_lookup.result["infra-media-secrets_radarr_api_key"]
   sync_categories = [2000, 2010, 2030]
 }
@@ -21,7 +21,7 @@ resource "prowlarr_download_client_sabnzbd" "sabnzbd" {
   enable   = true
   priority = 1
   name     = "sabnzbd"
-  host     = "sabnzbd.${var.cluster_media_domain}"
+  host     = "sabnzbd-app.${var.cluster_media_domain}"
   url_base = "/"
   port     = var.ports["sabnzbd"]
   category = "prowlarr"
