3c15745bc7efe8053183729a48c6c39ac94e598a
Bryan Jones <unix.chkpwd@gmail.com>
2025-06-22 22:01:04 -0400

    chore(gravity): rearrange resources

--- terraform/gravity/dns.tf
+++ terraform/gravity/dns.tf
@@ -1,3 +1,21 @@
+resource "gravity_dns_zone" "forward" {
+  # Root zone, will be used for all queries that don't match other zones
+  name = "."
+  handler_configs = jsonencode([
+    {
+      type = "memory",
+    },
+    {
+      type = "etcd",
+    },
+    {
+      type      = "forward_ip",
+      to        = ["1.1.1.1", "1.0.0.1"],
+      cache_ttl = 3600
+    },
+  ])
+}
+
 resource "gravity_dns_zone" "main" {
   name          = var.zone
   authoritative = true
@@ -25,33 +43,6 @@ resource "gravity_dns_zone" "main-rev" {
     },
     {
       type = "etcd",
-    },
-  ])
-}
-
-# resource "gravity_dns_record" "main-rev-device" {
-#   zone     = gravity_dns_zone.main-rev.id
-#   hostname = "${split(".", each.value)[3]}.${split(".", each.value)[2]}"
-#   uid      = var.default_uid
-#   data     = "${each.key}.${gravity_dns_zone.net-io.name}"
-#   type     = "PTR"
-#   for_each = gravity_dhcp_lease.lan_reservations
-# }
-
-resource "gravity_dns_zone" "forward" {
-  # Root zone, will be used for all queries that don't match other zones
-  name = "."
-  handler_configs = jsonencode([
-    {
-      type = "memory",
-    },
-    {
-      type = "etcd",
-    },
-    {
-      type      = "forward_ip",
-      to        = ["1.1.1.1", "1.0.0.1"],
-      cache_ttl = 3600
     },
   ])
 }
