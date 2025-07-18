e9d80ded2e381631ce0754b971eb3870df8094c3
Bryan Jones <unix.chkpwd@gmail.com>
2025-07-18 13:30:14 -0400

    fix(gravity): removed state on root zone

--- terraform/gravity/dns.tf
+++ terraform/gravity/dns.tf
@@ -4,12 +4,6 @@ resource "gravity_dns_zone" "root" {
   default_ttl   = 86400
   handler_configs = jsonencode([
     {
-      type = "memory",
-    },
-    {
-      type = "etcd",
-    },
-    {
       cache_ttl = 3600
       to        = ["8.8.8.8:53"]
       type      = "forward_blocky"
