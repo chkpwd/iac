a9c1276562bda83cc4aad12ec5514d86f2ba1607
Bryan Jones <unix.chkpwd@gmail.com>
2025-10-08 13:25:10 -0400

    deploy immich

--- kubernetes/apps/immich/database-init-cm.sql
+++ kubernetes/apps/immich/database-init-cm.sql
@@ -1,9 +1,2 @@
-CREATE DATABASE immich;
-\c immich
-BEGIN;
-ALTER DATABASE immich OWNER TO immich;
-CREATE EXTENSION IF NOT EXISTS earthdistance CASCADE; -- installs dependencies (i.e. cube)
-CREATE EXTENSION IF NOT EXISTS vectors;
-ALTER DATABASE immich SET search_path TO "$user", public, vectors;
-ALTER SCHEMA vectors OWNER TO immich;
-COMMIT;
+/c immich\\
+CREATE EXTENSION IF NOT EXISTS vchord CASCADE;
